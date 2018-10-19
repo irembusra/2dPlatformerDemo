7bdf6e4127be7db3abf4931b5d1645bf218f5ff3
İrem Kılıç <irembusraklc94@gmail.com>
2018-10-19 20:03:35 +0300

    new prefabs and killbox added

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour {
+
+    private PlayerMotor player;
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMotor>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            player.i_CurHealth = 0;
+        }
+    }
+}
